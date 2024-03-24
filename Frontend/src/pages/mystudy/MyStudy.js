0be22cc9ff83dbc0a900b51664c520ae142bb0ea
haeburney <4ckdnjssu@naver.com>
2024-03-24 13:18:29 +0900

    20240324 김혜원 : Header 헤더 수정, App.js router 수정, package axios 설치, MyPage 마이페이지 신규 개발, MyStudy 마이스터디 신규 개발

---
+++ Frontend/src/pages/mystudy/MyStudy.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyStudy extends Component {
+    render() {
+        return (
+            <div>
+                나의 스터디
+            </div>
+        );
+    }
+}
+
+export default MyStudy;
