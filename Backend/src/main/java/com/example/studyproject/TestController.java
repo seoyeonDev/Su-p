32e9b4a0e0232d8a4ad144ef002457b5dfb646e7
haeburney <4ckdnjssu@naver.com>
2024-04-06 15:31:07 +0900

    react spring 값 보내고 받아오는 코드 작성

--- Backend/src/main/java/com/example/studyproject/TestController.java
+++ Backend/src/main/java/com/example/studyproject/TestController.java
@@ -1,7 +1,9 @@
 package com.example.studyproject;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,5 +18,15 @@ public class TestController {
     @GetMapping("/test")
     public List<String> hello(){
         return Arrays.asList("하이1", "하이2");
+    }
+
+
+    @PostMapping("/test/saveData")
+    public String saveData(@RequestBody String inputValue) {
+        // 입력값 처리
+        System.out.println("입력값: " + inputValue);
+
+        // 필요한 작업 수행 후 응답 반환
+        return "데이터 저장 완료";
     }
 }
--- Frontend/src/routes/Home.js
+++ Frontend/src/routes/Home.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Home() {
     const [message, setMessage] = useState([]);
 
     useEffect(() => {
-        fetch("/test")
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                setMessage(data);
-            })
+        
+        axios.get('/test')
+        .then((response)=>{
+            setMessage(response.data);
+        })
+        .catch(error=>{
+            console.error('Error :',error);
+        })
+
+
     }, []);
+
+    // input 값 이 변수에 넣을거임
+    const [inputValue, setInputValue] = useState('');
+
+    // input 입력값이 바뀔 때마다 inputValue에 값 저장 
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    // 제출 버튼 눌렀을 때 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(inputValue);
+
+        // post('보낼 url', {보낼 데이터})
+        axios.post('/test/saveData',{inputValue})
+            .then(response=>{
+                console.log('서버 응답 : ', response.data);
+            })
+            .catch(error => {
+                console.error('error :', error);
+            })
+
+    }
 
 
     return (
@@ -26,6 +55,14 @@ function Home() {
             </div>
             <div>
                 위 인사는 스프링에서 가져온 것. 
+            </div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <input type="text" value={inputValue} onChange={handleChange}/>
+                    </label>
+                    <button type="submit">제출</button>
+                </form>
             </div>
         </div>
 
