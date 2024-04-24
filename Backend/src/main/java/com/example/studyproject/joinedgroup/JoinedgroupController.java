7d5989b484808b7f847f6908f90d71846d0ab28f
haeburney <4ckdnjssu@naver.com>
2024-04-24 20:10:44 +0900

    #54 : 김혜원 : joined group : 가입된 그룹 vo, service, controller 생성

---
+++ Backend/src/main/java/com/example/studyproject/joinedgroup/JoinedgroupController.java
@@ -0,0 +1,9 @@
+package com.example.studyproject.joinedgroup;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/joinedgroup")
+public class JoinedgroupController {
+}
---
+++ Backend/src/main/java/com/example/studyproject/joinedgroup/JoinedgroupDao.java
@@ -0,0 +1,7 @@
+package com.example.studyproject.joinedgroup;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface JoinedgroupDao {
+}
