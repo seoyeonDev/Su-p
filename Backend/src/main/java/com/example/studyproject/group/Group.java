c4a9164fe026790075eea13184aea26bca2f8cd0
SUNHO <sh525293@gmail.com>
2024-04-24 11:20:40 +0900

    issue #49 : 선호 : group : 그룹 추가 기본 코드 및 쿼리 생성

--- Backend/src/main/java/com/example/studyproject/group/Group.java
+++ Backend/src/main/java/com/example/studyproject/group/Group.java
@@ -25,4 +25,147 @@ public class Group {
     private char chk_m;         // 제출기준
     private String chk_d;       // 제출기준상세
     private int chk_cnt;        // 전체제출회수
+	
+    public Group() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Group(String group_id, Member leader_id, String title, String name, String desc, String kind, boolean status,
+			int mem_cnt, Timestamp startdate, Timestamp enddate, int view_cnt, char chk_m, String chk_d, int chk_cnt) {
+		this.group_id = group_id;
+		this.leader_id = leader_id;
+		this.title = title;
+		this.name = name;
+		this.desc = desc;
+		this.kind = kind;
+		this.status = status;
+		this.mem_cnt = mem_cnt;
+		this.startdate = startdate;
+		this.enddate = enddate;
+		this.view_cnt = view_cnt;
+		this.chk_m = chk_m;
+		this.chk_d = chk_d;
+		this.chk_cnt = chk_cnt;
+	}
+
+	public String getGroup_id() {
+		return group_id;
+	}
+
+	public void setGroup_id(String group_id) {
+		this.group_id = group_id;
+	}
+
+	public Member getLeader_id() {
+		return leader_id;
+	}
+
+	public void setLeader_id(Member leader_id) {
+		this.leader_id = leader_id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getKind() {
+		return kind;
+	}
+
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public int getMem_cnt() {
+		return mem_cnt;
+	}
+
+	public void setMem_cnt(int mem_cnt) {
+		this.mem_cnt = mem_cnt;
+	}
+
+	public Timestamp getStartdate() {
+		return startdate;
+	}
+
+	public void setStartdate(Timestamp startdate) {
+		this.startdate = startdate;
+	}
+
+	public Timestamp getEnddate() {
+		return enddate;
+	}
+
+	public void setEnddate(Timestamp enddate) {
+		this.enddate = enddate;
+	}
+
+	public int getView_cnt() {
+		return view_cnt;
+	}
+
+	public void setView_cnt(int view_cnt) {
+		this.view_cnt = view_cnt;
+	}
+
+	public char getChk_m() {
+		return chk_m;
+	}
+
+	public void setChk_m(char chk_m) {
+		this.chk_m = chk_m;
+	}
+
+	public String getChk_d() {
+		return chk_d;
+	}
+
+	public void setChk_d(String chk_d) {
+		this.chk_d = chk_d;
+	}
+
+	public int getChk_cnt() {
+		return chk_cnt;
+	}
+
+	public void setChk_cnt(int chk_cnt) {
+		this.chk_cnt = chk_cnt;
+	}
+
+	@Override
+	public String toString() {
+		return "Group [group_id=" + group_id + ", leader_id=" + leader_id + ", title=" + title + ", name=" + name
+				+ ", desc=" + desc + ", kind=" + kind + ", status=" + status + ", mem_cnt=" + mem_cnt + ", startdate="
+				+ startdate + ", enddate=" + enddate + ", view_cnt=" + view_cnt + ", chk_m=" + chk_m + ", chk_d="
+				+ chk_d + ", chk_cnt=" + chk_cnt + "]";
+	}
+    
 }
