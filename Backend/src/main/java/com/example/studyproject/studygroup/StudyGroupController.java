457993f9399f98f9554896096bfd95bc4125bcd3
seoyeonDev <skylarlee1003@gmail.com>
2024-06-03 01:11:37 +0900

    #138 : 이서연 : Studygroup : 그룹상태변경 메서드 및 스케줄러 - StudyGroupManage_SQL.xml 파일에서 selectListStudyGroup, createGroup 에러 발생하여 주석처리

--- Backend/src/main/java/com/example/studyproject/studygroup/StudyGroupController.java
+++ Backend/src/main/java/com/example/studyproject/studygroup/StudyGroupController.java
@@ -9,7 +9,7 @@ package com.example.studyproject.studygroup;
  * @ -----------    --------    ---------------------------
  * @ 2024.05.01     봉선호        최초 생성
  * @ 2024.05.17		봉선호		그룹 추가 메서드 수정
- *
+ * @ 2024.06.02     이서연        그룹상태 변경 스케줄러 및 메서드 추가
  */
 
 import java.security.NoSuchAlgorithmException;
@@ -23,58 +23,63 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@EnableScheduling
+@SpringBootApplication
 @RestController
-@RequestMapping("/group")
+@RequestMapping("/studygroup")
 public class StudyGroupController {
-	
-	@Autowired
-	private StudyGroupService groupService;
-	
-	// log4j2 로그 찍기
-	private static final Logger LOGGER = LogManager.getLogger(StudyGroupController.class);
-	
+
+    @Autowired
+    private StudyGroupService groupService;
+
+    // log4j2 로그 찍기
+    private static final Logger LOGGER = LogManager.getLogger(StudyGroupController.class);
+
     /**
      * 시퀀스 생성 및 전체체출횟수 계산해서 그룹 추가 & JoinedGroup에 그룹장 추가
-	 * @param vo - StudyGroup vo
-	 */
-	@PostMapping("/createGroup")
-	public void createGroup(@RequestBody StudyGroup vo) throws NoSuchAlgorithmException {
-		
-		// 그룹장의 id는 로그인 시 세션에 아이디 저장하고, 프론트단에서 아이디 세션 불러서 vo로 넘기기
-		// 그룹ID 240517 + 0001 형식 로직.. 가장 큰 숫자 불러와서 없으면 만들어주고 있으면 +1
-		LocalDateTime now = LocalDateTime.now();
-		String year = String.valueOf(now.getYear());
-		String month = groupService.addZero(now.getMonthValue());
-		String day = groupService.addZero(now.getDayOfMonth());
-		String yrmd = year.substring(2, 3) + month + day;
-		
-		String maxGroupId = groupService.getMaxGroupId(yrmd);
-		if(!maxGroupId.equals("") && maxGroupId != null) {
-			int nextValue = Integer.valueOf(maxGroupId) + 1;
-			maxGroupId = String.valueOf(nextValue);
-			vo.setGroup_id(maxGroupId);
-		} else {
-			maxGroupId = yrmd + "0001";
-			vo.setGroup_id(maxGroupId);
-		}
-		
-		// 총제출횟수
-		// 자바 타임 패키지 사용하여 날짜와 날짜 사이의 일 차이값, 월 차이값 구하기
-		// 주단위: (종료일-시작일)/7 * 최소제출횟수
-		// 월단위: 자바스크립트 Date util setMonth 기준 두 날짜 사이의 월 차이
+     * @param vo - StudyGroup vo
+     */
+    @PostMapping("/createGroup")
+    public void createGroup(@RequestBody StudyGroup vo) throws NoSuchAlgorithmException {
+
+        // 그룹장의 id는 로그인 시 세션에 아이디 저장하고, 프론트단에서 아이디 세션 불러서 vo로 넘기기
+        // 그룹ID 240517 + 0001 형식 로직.. 가장 큰 숫자 불러와서 없으면 만들어주고 있으면 +1
+        LocalDateTime now = LocalDateTime.now();
+        String year = String.valueOf(now.getYear());
+        String month = groupService.addZero(now.getMonthValue());
+        String day = groupService.addZero(now.getDayOfMonth());
+        String yrmd = year.substring(2, 3) + month + day;
+
+        String maxGroupId = groupService.getMaxGroupId(yrmd);
+        if (!maxGroupId.equals("") && maxGroupId != null) {
+            int nextValue = Integer.valueOf(maxGroupId) + 1;
+            maxGroupId = String.valueOf(nextValue);
+            vo.setGroup_id(maxGroupId);
+        } else {
+            maxGroupId = yrmd + "0001";
+            vo.setGroup_id(maxGroupId);
+        }
+
+        // 총제출횟수
+        // 자바 타임 패키지 사용하여 날짜와 날짜 사이의 일 차이값, 월 차이값 구하기
+        // 주단위: (종료일-시작일)/7 * 최소제출횟수
+        // 월단위: 자바스크립트 Date util setMonth 기준 두 날짜 사이의 월 차이
         LocalDateTime startDateTime = vo.getStartdate();
         LocalDateTime endDateTime = vo.getEnddate();
 
         LocalDate startDate = startDateTime.toLocalDate();
         LocalDate endDate = endDateTime.toLocalDate();
-		
+
         int totCnt = 0;
         if ((vo.getChk_m()).equals("SUBM10")) {
             long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
@@ -86,26 +91,26 @@ public class StudyGroupController {
             vo.setChk_total_cnt(totCnt);
         }
 
-		groupService.createGroup(vo);
-		
-		// JoinedGroup에 그룹장ID로 동시에 추가
-		// 혜원 pr 후 추가
-	}
-	
-	//  그룹 수정
-	@PutMapping("(/updateGroup")
-	public void updateGroup(@RequestBody StudyGroup vo) {
+        groupService.createGroup(vo);
 
-		// 총제출횟수
-		// 자바 타임 패키지 사용하여 날짜와 날짜 사이의 일 차이값, 월 차이값 구하기
-		// 주단위: (종료일-시작일)/7 * 최소제출횟수
-		// 월단위: 자바스크립트 Date util setMonth 기준 두 날짜 사이의 월 차이
+        // JoinedGroup에 그룹장ID로 동시에 추가
+        // 혜원 pr 후 추가
+    }
+
+    //  그룹 수정
+    @PutMapping("(/updateGroup")
+    public void updateGroup(@RequestBody StudyGroup vo) {
+
+        // 총제출횟수
+        // 자바 타임 패키지 사용하여 날짜와 날짜 사이의 일 차이값, 월 차이값 구하기
+        // 주단위: (종료일-시작일)/7 * 최소제출횟수
+        // 월단위: 자바스크립트 Date util setMonth 기준 두 날짜 사이의 월 차이
         LocalDateTime startDateTime = vo.getStartdate();
         LocalDateTime endDateTime = vo.getEnddate();
 
         LocalDate startDate = startDateTime.toLocalDate();
         LocalDate endDate = endDateTime.toLocalDate();
-		
+
         int totCnt = 0;
         if ((vo.getChk_m()).equals("SUBM10")) {
             long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
@@ -117,25 +122,35 @@ public class StudyGroupController {
             vo.setChk_total_cnt(totCnt);
         }
 
-		groupService.updateGroup(vo);
-	}
-	
-  	// 그룹 목록 호출
-	@PostMapping("/studyGroupList")
-	public void studyGroupList() {
+        groupService.updateGroup(vo);
+    }
 
-		LOGGER.info("======================= 리스트 포스트 호출");
+    // 그룹 목록 호출
+    @PostMapping("/studyGroupList")
+    public void studyGroupList() {
 
-		List<?> studyGroupList = groupService.selectListStudyGroup();
-		LOGGER.info("studyGroup 리스트: " + studyGroupList);
-		
-		// 리턴 변수 클라이언트단 작업하면서 수정
-  }  
-  
-	// 그룹 삭제
-	@Delete("/deleteGroup")
-	public void deleteGroup(@RequestBody StudyGroup vo) {
+        LOGGER.info("======================= 리스트 포스트 호출");
 
-		groupService.deleteGroup(vo);
-	}
+        List<?> studyGroupList = groupService.selectListStudyGroup();
+        LOGGER.info("studyGroup 리스트: " + studyGroupList);
+
+        // 리턴 변수 클라이언트단 작업하면서 수정
+    }
+
+    // 그룹 삭제
+    @Delete("/deleteGroup")
+    public void deleteGroup(@RequestBody StudyGroup vo) {
+
+        groupService.deleteGroup(vo);
+    }
+
+
+    // 그룹 상태 변경
+    @Scheduled(cron = "0 0 0 * * *")
+//    @Scheduled(cron = "0 0/1 * * * ?")    // 테스트용
+    @PostMapping("/changeStatus")
+    public void changeStatus() {
+        groupService.changeStatus();
+        LOGGER.info("Group Change Status success");
+    }
 }
