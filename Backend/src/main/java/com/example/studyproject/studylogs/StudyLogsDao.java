8c4bb14376759bf887e7705c9c83cefe39fb9fbd
seoyeonDev <skylarlee1003@gmail.com>
2024-06-03 23:03:34 +0900

    #141 : 이서연 : studylogs : 결과물 신규추가 및 테이블 DDL 수정 - post_id 를 group_id 에 따라 SEQUENce 를 적용하여 테이블 pk 값 추가

--- Backend/src/main/java/com/example/studyproject/studylogs/StudyLogsDao.java
+++ Backend/src/main/java/com/example/studyproject/studylogs/StudyLogsDao.java
@@ -4,4 +4,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface StudyLogsDao {
 
+    // 결과물 신규추가
+    void insertLogs(StudyLogs vo);
 }
--- Backend/src/main/resources/SQL/DDL/studylogs_table.sql
+++ Backend/src/main/resources/SQL/DDL/studylogs_table.sql
@@ -33,3 +33,9 @@ comment on column studylogs.file_id is '파일명';
 
 comment on column studylogs.img_id is '이미지';
 
+create unique index studylogs_pk
+    on public.studylogs (post_id);
+
+alter table public.studylogs
+    add constraint studylogs_pk
+        primary key (post_id, group_id);
