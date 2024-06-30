bd3c9163d98abea6d3f172500afb1fc179565135
haeburney <4ckdnjssu@naver.com>
2024-07-01 01:37:02 +0900

    #223 : 김혜원 : penalty 조회 : penalty userId, groupId 별로 조회 기능

--- Backend/src/main/java/com/example/studyproject/penaltylog/PenaltylogDao.java
+++ Backend/src/main/java/com/example/studyproject/penaltylog/PenaltylogDao.java
@@ -1,8 +1,16 @@
 package com.example.studyproject.penaltylog;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.ArrayList;
 
 @Mapper
 public interface PenaltylogDao {
+    // 유저 아이디로 penaltylog 불러오기
+    ArrayList<Penaltylog> getByUserId(@Param("user_id") String user_id);
+
+    // 그룹 아이디로 penaltylog 불러오기
+    ArrayList<Penaltylog> getByGroupId(@Param("group_id") String group_id);
 
 }
