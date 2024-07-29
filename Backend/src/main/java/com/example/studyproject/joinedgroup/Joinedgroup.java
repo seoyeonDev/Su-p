161dfc4838319077ca5ab4e0ed91b5b96909145f
SUNHO <sh525293@gmail.com>
2024-07-29 23:02:49 +0900

    issue#293 : 봉선호 : studygroup : 스터디그룹 생성 - 리액트와 날짜 포맷 맞춰주기 위해 vo 설정 추가

--- Backend/src/main/java/com/example/studyproject/joinedgroup/Joinedgroup.java
+++ Backend/src/main/java/com/example/studyproject/joinedgroup/Joinedgroup.java
@@ -16,4 +16,64 @@ public class Joinedgroup {
     private String role;
     private String joinstatus;
     private int submission_cnt;
+
+//    public Joinedgroup() {
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	public Joinedgroup(String group_id, String user_id, String role, String joinstatus, int submission_cnt) {
+//		super();
+//		this.group_id = group_id;
+//		this.user_id = user_id;
+//		this.role = role;
+//		this.joinstatus = joinstatus;
+//		this.submission_cnt = submission_cnt;
+//	}
+//
+//	public String getGroup_id() {
+//		return group_id;
+//	}
+//
+//	public void setGroup_id(String group_id) {
+//		this.group_id = group_id;
+//	}
+//
+//	public String getUser_id() {
+//		return user_id;
+//	}
+//
+//	public void setUser_id(String user_id) {
+//		this.user_id = user_id;
+//	}
+//
+//	public String getRole() {
+//		return role;
+//	}
+//
+//	public void setRole(String role) {
+//		this.role = role;
+//	}
+//
+//	public String getJoinstatus() {
+//		return joinstatus;
+//	}
+//
+//	public void setJoinstatus(String joinstatus) {
+//		this.joinstatus = joinstatus;
+//	}
+//
+//	public int getSubmission_cnt() {
+//		return submission_cnt;
+//	}
+//
+//	public void setSubmission_cnt(int submission_cnt) {
+//		this.submission_cnt = submission_cnt;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Joinedgroup [group_id=" + group_id + ", user_id=" + user_id + ", role=" + role + ", joinstatus="
+//				+ joinstatus + ", submission_cnt=" + submission_cnt + "]";
+//	}
+    
 }
