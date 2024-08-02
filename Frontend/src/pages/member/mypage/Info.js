9a1fdcefbc0128dc8f519fdb700910f086ecb71a
SUNHO <sh525293@gmail.com>
2024-08-03 00:04:17 +0900

    293

--- Frontend/src/pages/member/mypage/Info.js
+++ Frontend/src/pages/member/mypage/Info.js
@@ -24,7 +24,7 @@ const Info = () => {
     }, [emailTime])
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchData = async () => { 
             try {
                 const response = await axios.get('http://localhost:3000/member/mypage', {
                     params: {
