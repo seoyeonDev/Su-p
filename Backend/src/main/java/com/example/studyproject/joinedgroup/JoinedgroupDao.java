1b6864df512c1f4b766d27da832543aa46534c3a
SUNHO <sh525293@gmail.com>
2024-08-11 21:04:33 +0900

    issue #314 : 봉선호 : studygroup : 그룹 상세보기 - 수정, 삭제

--- Backend/src/main/java/com/example/studyproject/joinedgroup/JoinedgroupDao.java
+++ Backend/src/main/java/com/example/studyproject/joinedgroup/JoinedgroupDao.java
@@ -40,4 +40,7 @@ public interface JoinedgroupDao {
 
     // 조건에 따른 목록보기
     ArrayList<Joinedgroup> selectList(String joinstatus, String role, String status, String user_id);
+    
+    // 그룹아이디로 리스트 사이즈 체크
+    int selectJoinedListSize(String group_id);
 }
