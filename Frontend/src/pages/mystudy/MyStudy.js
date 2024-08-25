f656557ae5c8562faf2fc508610bb2a0109e381a
haeburney <4ckdnjssu@naver.com>
2024-08-25 23:27:46 +0900

    #323 : 김혜원 : studygroup 틀 만들기 : studygroup 메뉴 틀 만들기

--- Frontend/src/pages/mystudy/MyStudy.js
+++ Frontend/src/pages/mystudy/MyStudy.js
@@ -1,13 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import '../../styles/studygroup/MyStudy.css'
+// import axios from 'axios';
 
-class MyStudy extends Component {
-    render() {
-        return (
-            <div>
-                나의 스터디
+function MyStudy() {
+    const [activeButton, setActiveButton] = useState('');
+
+    const handleButtonClick = (buttonName) => {
+        setActiveButton(buttonName);
+        console.log(buttonName);
+
+    };
+
+    return (
+        <div className='common-content-container'>
+            <div className='common-content'>
+                <h1>나의 스터디</h1>
+
+                <div className='tab-buttons'>
+                    <button
+                        className={`tab-button ${activeButton === '전체 스터디' ? 'active' : ''}`}
+                        onClick={() => handleButtonClick('전체 스터디')}
+                    >
+                        전체 스터디
+                    </button>
+                    <span>|</span>
+                    <button
+                        className={`tab-button ${activeButton === '참여중인 스터디' ? 'active' : ''}`}
+                        onClick={() => handleButtonClick('참여중인 스터디')}
+                    >
+                        참여중인 스터디
+                    </button>
+                    <span>|</span>
+                    <button
+                        className={`tab-button ${activeButton === '운영중인 스터디' ? 'active' : ''}`}
+                        onClick={() => handleButtonClick('운영중인 스터디')}
+                    >
+                        운영중인 스터디
+                    </button>
+                    <span>|</span>
+                    <button
+                        className={`tab-button ${activeButton === '종료된 스터디' ? 'active' : ''}`}
+                        onClick={() => handleButtonClick('종료된 스터디')}
+                    >
+                        종료된 스터디
+                    </button>
+                </div>
+
+                <div className="attendance-rate">
+                    <div> </div>
+                       
+                    
+                </div>
+
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default MyStudy;
