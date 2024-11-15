ff7e83a967be1990071ca7112117ae16a7a0c44a
haeburney <4ckdnjssu@naver.com>
2024-11-16 03:26:49 +0900

    #355 : 김혜원 : studylogs 글 생성 및 수정 : studylogs 글 생성 및 수정

---
+++ Backend/src/main/java/com/example/studyproject/files/SupFilesInfo.java
@@ -0,0 +1,13 @@
+package com.example.studyproject.files;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class SupFilesInfo {
+    private String file_seq;
+    private String file_id;
+}
