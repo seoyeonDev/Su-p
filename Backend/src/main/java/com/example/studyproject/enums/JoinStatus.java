d2b8486825ef6305f384a77dab2f2d73899e485b
haeburney <4ckdnjssu@naver.com>
2024-11-16 14:13:50 +0900

    #358 : 김혜원 : JoinStatus enum 추가 :  JoinStatus enum 추가

---
+++ Backend/src/main/java/com/example/studyproject/enums/JoinStatus.java
@@ -0,0 +1,10 @@
+package com.example.studyproject.enums;
+
+public enum JoinStatus {
+    ROLE10, // 그룹장
+    ROLE20, // 참여자
+    PERM10, // 가입대기 JOIN_REQUEST
+    PERM20, // 가입승인 JOIN_APPROVED
+    PERM30, // 가입거절
+    PERM40 // 강퇴
+}
--- Backend/src/main/java/com/example/studyproject/joinedgroup/JoinedgroupDao.java
+++ Backend/src/main/java/com/example/studyproject/joinedgroup/JoinedgroupDao.java
@@ -1,5 +1,6 @@
 package com.example.studyproject.joinedgroup;
 
+import com.example.studyproject.enums.JoinStatus;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
  * @ 2024.05.27     김혜원        get 메서드 추가
  * @ 2024.06.10     이서연        사용자별 가입한 그룹 목록 메서드 추가
  * @ 2024.06.24     이서연        조건에 따른 목록보기
+ * @ 2024.11.16     김혜원        JoinStstus enum 추가
  */
 @Mapper
 public interface JoinedgroupDao {
@@ -39,11 +41,11 @@ public interface JoinedgroupDao {
     Joinedgroup getByUserIdAndGroupId(String user_id, String group_id);
 
     // 조건에 따른 목록보기
-    ArrayList<Joinedgroup> selectList(String joinstatus, String role, String status, String user_id);
+    ArrayList<Joinedgroup> selectList(JoinStatus joinstatus, String role, String status, String user_id);
     
     // 그룹아이디로 리스트 사이즈 체크
     int selectJoinedListSize(String group_id);
 
     // 그룹 ID와 가입 상태에 따라 joinedgroup 목록을 조회합니다.
-    ArrayList<JoinedUserInfo> selectJoinedListByGroupId(String group_id,  String joinStatus);
+    ArrayList<JoinedUserInfo> selectJoinedListByGroupId(String group_id,  JoinStatus joinStatus);
 }
