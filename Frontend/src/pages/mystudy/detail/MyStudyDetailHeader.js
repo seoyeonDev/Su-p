ba02c770004095133bf3a884a127ab101132a62d
haeburney <4ckdnjssu@naver.com>
2024-12-21 23:54:05 +0900

    #409 : 김혜원 : mystudydetail : header css 추가

--- Frontend/src/pages/mystudy/detail/MyStudyDetailHeader.js
+++ Frontend/src/pages/mystudy/detail/MyStudyDetailHeader.js
@@ -2,6 +2,7 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom'; 
 import { useNavigate } from 'react-router-dom';
+import '../../../styles/studygroup/MyStudy.css';
 
 const MyStudyDetailHeader = ({title, onSelect, isAdmin, group_id}) => {
     const navigate = useNavigate();
